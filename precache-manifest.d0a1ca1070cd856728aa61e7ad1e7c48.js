c978baf5ac2a67ac19c793749cf40c151519e3d8
BTchorzewski <tchorzewskibartosz@gmail.com>
2019-05-27 20:46:42 +0200

    Updates

---
+++ precache-manifest.d0a1ca1070cd856728aa61e7ad1e7c48.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8657ff82d0a263415ed40ebf01baf3fd",
+    "url": "/weather_app/index.html"
+  },
+  {
+    "revision": "62aa5b836e9f1bf49a67",
+    "url": "/weather_app/static/css/main.0e51bb0b.chunk.css"
+  },
+  {
+    "revision": "daa772645ee8ec2802c0",
+    "url": "/weather_app/static/js/2.10a2e262.chunk.js"
+  },
+  {
+    "revision": "62aa5b836e9f1bf49a67",
+    "url": "/weather_app/static/js/main.47907d41.chunk.js"
+  },
+  {
+    "revision": "23fd1cd65a8563fa95bd",
+    "url": "/weather_app/static/js/runtime~main.17b50636.js"
+  },
+  {
+    "revision": "177764eadf43efc393ebb98329a774b2",
+    "url": "/weather_app/static/media/linea-basic-10.177764ea.svg"
+  },
+  {
+    "revision": "61f3a8a387a64abfc0535cd0813cd71a",
+    "url": "/weather_app/static/media/linea-basic-10.61f3a8a3.woff"
+  },
+  {
+    "revision": "e4de528c00da0c081906c3caea289f0f",
+    "url": "/weather_app/static/media/linea-basic-10.e4de528c.eot"
+  },
+  {
+    "revision": "e6763f008205c55db623cdf68bbd581f",
+    "url": "/weather_app/static/media/linea-basic-10.e6763f00.ttf"
+  }
+]);
